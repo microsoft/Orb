83bf8419bb7aa96d2dc70bcd6dab4e7a5c7e1118
Gaurav Jagtiani <gajagt@ntdev.microsoft.com>
2018-11-21 18:14:21 -0800

    Add initial gulp file.

---
+++ build/gulpfile.js
@@ -0,0 +1,37 @@
+var gutil = require("gulp-util");
+
+var paths = {
+	electron: ["node_modules/electron/dist/**/*", "!/**/electron.exe"],
+	electronExe: ["node_modules/electron/dist/electron.exe"],
+	updateExe: ["node_modules/electron-winstaller/vendor/update.exe"],
+	winstaller: "node_modules/electron-winstaller/",
+	assets: "assets",
+	dist: "dist",
+	app: "dist/resources/app",
+	build: "build"
+}
+
+gulp.task("copyElectron", function () {
+	gutil.log("Copying electron dependencies.");
+	return gulp.src(paths.electron)
+		.pipe(gulp.dest(paths.dist));
+});
+
+gulp.task("renameElectronExe", function () {
+
+	var exeName = getExeName();
+	gutil.log("Copying electron exe and renaming to " + exeName);
+	return gulp.src(paths.electronExe)
+		.pipe(rename(exeName))
+		.pipe(gulp.dest(paths.dist));
+});
+
+function getExeName() {
+    var exeName = "orb.exe";
+    gutil.log("Running on branch " + argv.buildBranch);
+    return exeName;
+}
+
+gulp.task("build", ["copyElectron", "renameElectronExe"], function (callback) {
+	gutil.log("Building Orb");
+});
