00d13474313a7b15d18d9bb34057093102dd4114
Gaurav Jagtiani <gajagt@ntdev.microsoft.com>
2018-12-17 12:40:47 -0800

    Build fix

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,3 +1,6 @@
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
 var gulp = require("gulp");
 var gutil = require("gulp-util");
 var argv = require('yargs').argv;
